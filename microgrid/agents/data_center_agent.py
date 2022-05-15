a03b526b36ed330800f33c86ea7ba2b777b9b917
Eliel DZIK <eliel.dzik@enpc.fr>
2022-05-15 23:40:07 +0200

    mise en place de l'agent

--- microgrid/agents/data_center_agent.py
+++ microgrid/agents/data_center_agent.py
@@ -1,27 +1,28 @@
 import datetime
 from microgrid.environments.data_center.data_center_env import DataCenterEnv
-
+from microgrid.assets.data_center import DataCenter
 
 import pandas as pd
 from pulp import *
 import numpy as np
 
 class DataCenterAgent:
-    def __init__(self, env: DataCenterEnv):
+    def __init__(self, env: DataCenterEnv, Dt : DataCenter):
         self.env = env
+        self.Dt = Dt
 
     def take_decision(self,
                       state,
                       previous_state=None,
                       previous_action=None,
                       previous_reward=None):
-            l_IT = np.array([l_IT_global[i + time]] for i in range(
-                24))  # on étudie le scénario à partir de la consommation minimale entre t et t+24h
-            lambdas = self.env.get_prices_prevision()
+            l_IT = np.array([self.Dt.data[i+ self.env.now]] for i in range(
+                self.env.nb_pdt))  # on étudie le scénario à partir de la consommation minimale entre t et t+24h
+            lambdas = self.env.observation_space["manager_signal"]
+            phw = self.Dt.prices
             problem = LpProblem("data_center", LpMinimize)
             alphas = [0 for i in range(self.env.nb_pdt)]
             alpha = [0 for i in range(self.env.nb_pdt)]
-            LI = [0 for i in range(self.env.nb_pdt)]
             for i in range(24):
                 var_name = "alpha_" + str(i)
                 alphas[i] = LpVariable(var_name, 0.0, 1.0)
@@ -32,7 +33,7 @@ class DataCenterAgent:
             h_DC = [0 for i in range(self.env.nb_pdt)]
             li = [0 for i in range(self.env.nb_pdt)]
 
-            for t in range(self.env.horizon):
+            for t in range(self.env.nb_pdt):
                 l_NF[t] = (1 + 1 / (self.env.EER * self.env.delta_t)) * l_IT[t]
                 h_r[t] = l_IT[t] * self.env.COP_CS / self.env.EER
                 l_HP[t] = alphas[t] * h_r[t] / ((self.env.COP_HP - 1) * dt)
@@ -41,14 +42,14 @@ class DataCenterAgent:
                 problem += h_DC[t] <= self.env.max_transfert
                 li[t] = l_HP[t] + l_NF[t]
 
-            problem += np.sum([lambdas[i] * (l_NF[i] + h_DC[i]) - self.env.hotwater_price_prevision[i] * h_DC[i] for i in
+            problem += np.sum([lambdas[i] * (l_NF[i] + h_DC[i]) - phw[i] * h_DC[i] for i in
                                range(self.env.nb_pdt)]), "objectif"
 
             problem.solve()
             for i in range(48):
                 alpha[i] = alphas[i].value()
             self.env.action_space = alpha
-        return self.env.action_space.sample()
+            return self.env.action_space.sample()
 
 
 if __name__ == "__main__":
