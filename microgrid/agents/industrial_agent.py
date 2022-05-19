5e35b615c3eb8478f2d4a2903a56a3cfa87f53d0
eliel <93941672+eloulili@users.noreply.github.com>
2022-05-19 23:02:21 +0200

    Copie équipe smart grid

--- microgrid/agents/industrial_agent.py
+++ microgrid/agents/industrial_agent.py
@@ -6,12 +6,67 @@ class IndustrialAgent:
     def __init__(self, env: IndustrialEnv):
         self.env = env
 
-    def take_decision(self,
+   def take_decision(self,
                       state,
                       previous_state=None,
                       previous_action=None,
                       previous_reward=None):
-        return self.env.action_space.sample()
+        
+        #données du manager
+        consumption_prevision = state.get("consumption_prevision") #la demande de consommation
+        soc = state.get("soc") #à utiliser au 2nd run, pas aujourd'hui
+        manager_signal = state.get("manager_signal") #les prix
+        date_time = state.get("datetime") #à utiliser au 2nd run, pas aujourd'hui
+        
+        #constantes batterie
+        pmax = self.env.battery.pmax #puissance maximale de la batterie
+        efficiency = self.env.battery.efficiency #rendement de la batterie, le même pour la charge et la décharge
+        capacity = self.env.battery.capacity #charge maximale
+        a_tdec = self.env.battery.soc #state[soc]
+
+        #constantes temps
+
+        delta_t = datetime.timedelta(minutes=30)
+        H = datetime.timedelta(hours=1)
+        T = self.env.nb_pdt #Nombre de périodes temporelles
+        liste_temps = [t for t in range(T)]
+
+        #création du problème
+
+        prob = pl.LpProblem("industrial_site", pl.LpMinimize)
+
+        #définition des variables
+
+        a = pl.LpVariable.dicts("batterie_stock",liste_temps,0,capacity)
+        l_bat_plus = pl.LpVariable.dicts("l_batterie_plus",liste_temps,0)
+        l_bat_moins = pl.LpVariable.dicts("l_batterie_moins",liste_temps,0)
+        l_bat = pl.LpVariable.dicts("l_batterie",liste_temps)
+        l_tot = pl.LpVariable.dicts("l_demande_totale",liste_temps)
+
+        #Fonction objectif
+        
+        prob += pl.lpSum([l_tot[t] * manager_signal[t] * delta_t/H for t in liste_temps])
+
+        #contraintes
+
+        prob += a[0] == a_tdec
+        
+        for t in range(T):
+
+            prob += l_bat_plus[t] + l_bat_moins[t] <= pmax
+            prob += l_bat[t] == l_bat_plus[t] - l_bat_moins[t] #pas besoin de l_bat en variable pour la prochaine fois
+            prob += l_tot[t] == consumption_prevision[t] + l_bat[t] #pas besoin de l_tot en variable pour la prochaine fois
+            if t>0:
+                prob += a[t] == a[t-1] +(efficiency*l_bat_plus[t] - l_bat_moins[t]*1/efficiency)*delta_t/H
+
+
+        prob.solve()
+
+        result = self.env.action_space.sample()
+        for t in range(T):
+            result[t] = l_tot[t].value()
+
+        return result
 
 
 if __name__ == "__main__":
